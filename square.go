5914aeda403e69d5f88a966cf8837ea775c57bbe
Adam Kuzikov <kuzikov.dev@protonmail.com>
2022-06-03 16:48:31 +0600

    fix End() method

--- square.go
+++ square.go
@@ -15,20 +15,20 @@ type Square struct {
 func (r Square) End() Point {
 	return Point{
 		x: r.start.x + int(r.a),
-		y: r.start.y + int(r.a),
+		y: r.start.y - int(r.a),
 	}
 }
 
 // Area ...
 func (r Square) Area() uint {
-	a := r.End().x - r.start.x
-	b := r.End().y - r.start.y
-	return uint(a * b)
+	// a := r.End().x - r.start.x
+	// b := r.End().y - r.start.y
+	return uint(r.a * r.a)
 }
 
 // Perimeter ...
 func (r Square) Perimeter() uint {
-	a := r.End().x - r.start.x
-	b := r.End().y - r.start.y
-	return uint(a * b)
+	// a := r.End().x - r.start.x
+	// b := r.End().y - r.start.y
+	return uint(2 * r.a)
 }
